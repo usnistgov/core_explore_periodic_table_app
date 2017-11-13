55f8fc6ace16efeace9945f30e238945d428a468
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2017-11-13 08:03:04 -0500

    Update version alpha3
    Change-Id: I8b44e9e45f773c6956a6cbb69e65c7d18de62ec3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="core_explore_periodic_table_app",
-    version="1.0.0-alpha2",
+    version="1.0.0-alpha3",
     description=("core explore periodic table Django package",),
     author="NIST IT Lab",
     author_email="itl_inquiries@nist.gov",
