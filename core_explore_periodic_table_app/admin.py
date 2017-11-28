ee419fde0247127c00c5f3ac7b927eee5bd996c0
Philippe Dessauw <philippe.dessauw@nist.gov>
2017-11-27 20:07:34 -0500

    fix: clean reqs and imports
    Change-Id: Ib00d480fed82a88582702f5bcc47ff7aae7d6b57

--- core_explore_periodic_table_app/admin.py
+++ core_explore_periodic_table_app/admin.py
@@ -3,8 +3,7 @@ Url router for the administration site
 """
 from django.conf.urls import url
 from django.contrib import admin
-
-from views.admin import views as admin_views
+from core_explore_periodic_table_app.views.admin import views as admin_views
 
 admin_urls = [
     url(r'^periodic_table$', admin_views.manage_periodic_table_index, name='manage_periodic_table_index'),
