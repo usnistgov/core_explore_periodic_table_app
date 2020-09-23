3d7f2598bd94d78b1cc09a7c6020b916d0449109
Augustin <augustin.chini@nist.gov>
2020-09-23 15:43:21 -0400

    fix: remove unused setting
    Change-Id: I2d78eea0db0652ce13651124d1e09d4e98b931f7

--- core_explore_periodic_table_app/settings.py
+++ core_explore_periodic_table_app/settings.py
@@ -1,13 +1,6 @@
-"""Core Explore Period Table App Settings
+"""Core Explore Periodic Table App Settings
 """
 from django.conf import settings
 
 if not settings.configured:
     settings.configure()
-
-PERIODIC_TABLE_SEARCH_OPERATOR = getattr(
-    settings, "PERIODIC_TABLE_SEARCH_OPERATOR", "periodic_table"
-)
-""" :py:class:`string`: name of the field which notice that 
-     a search operator is compatible with the periodic table type path.
-"""
