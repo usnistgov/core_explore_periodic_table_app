0d4887d590afdad631025fa165411dda2686182b
Bouhanni <hnb8@pn120373.campus.nist.gov>
2021-01-26 10:58:44 -0500

    feat: add persistent rename query url
    Change-Id: I342a544239a182da9d07b94e8644c4439cfaac56

--- core_explore_periodic_table_app/static/core_explore_periodic_table_app/user/js/persistent_query.raw.js
+++ core_explore_periodic_table_app/static/core_explore_periodic_table_app/user/js/persistent_query.raw.js
@@ -1,1 +1,2 @@
 let persistentQueryUrl = "{% url 'core_explore_periodic_table_get_persistent_query_url' %}";
+let persistentRenameQueryUrl = "{% url 'core_explore_periodic_table_app_rest_persistent_query_periodic_table_detail' 'queryId' %}";
