4ff32e3b49b166964a4d2519eafa48b38a11160d
Guillaume Sousa <guillaume.sousa@nist.gov>
2022-07-07 18:15:51 +0000

    chore: upgrade 1.9.0
    Change-Id: Id02aa695a1ed83ee67c9d363fc9aa0cd61a815f7

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="core_explore_periodic_table_app",
-    version="1.8.0",
+    version="1.9.0",
     description="Core explore periodic table capabilities for the curator core project",
     long_description=long_desc,
     author="NIST IT Lab",
