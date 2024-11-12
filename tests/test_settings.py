b0bf96c2f7e901d4b5b350d7ac036a060a8b13c4
Guillaume-sousa <guillaume.sousa@nist.gov>
2024-11-12 15:45:08 -0500

    feat: django-allauth
    Change-Id: I3c9bd73fadd46bd9944f284134e8d5d3d92be22c

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -69,4 +69,5 @@ CELERYBEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
 MONGODB_INDEXING = False
 MONGODB_ASYNC_SAVE = False
 STATIC_URL = "/static/"
+ENABLE_ALLAUTH = False
 ENABLE_SAML2_SSO_AUTH = False
